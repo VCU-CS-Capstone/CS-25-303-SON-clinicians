6a0b26961dc41fe257f7b670408c1dfe9e249868
Wyatt Herkamp <wherkamp@gmail.com>
2025-01-28 19:14:54 -0500

    Added Frontend README.md file

--- src/frontend/app/[participant_id]/overview.tsx
+++ src/frontend/app/[participant_id]/overview.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { Button } from '~/components/Button';
 import LabelAndItem from '~/components/LabelAndItem';
-import RecentVisits from '~/components/participant/RecentVisits';
 
 import ProtectedRoute from '~/components/ProtectedRoute';
 import api from '~/lib/api';
